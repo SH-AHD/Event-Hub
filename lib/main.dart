2a7657f3018f08cd67775dabe666fe15c7ab1ab0
Mahmoud Abdelkader <muhamod39@gmail.com>
2026-02-21 13:48:53 +0200

    mmm

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,15 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: 
-      SearchScreen(),
-      // Scaffold(
-      //   body: Center(
-      //     child: Text('Hello Event Hub Team!'),
-      //   ),
-      // ),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello Event Hub Team!'),
+        ),
+      ),
     );
   }
 }
