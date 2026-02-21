fb53a1cd28f6c4855638a35b3c5a05b95c8c90e7
Mahmoud Abdelkader <muhamod39@gmail.com>
2026-02-21 23:48:36 +0200

    filter_pross

---
+++ lib/features/Home/Screens/search/data/category_list.dart
@@ -0,0 +1,17 @@
+import 'package:event_hub/core/constants/app_assets.dart';
+import 'package:flutter/material.dart';
+
+class CategoryModel {
+  final String category;
+  final String icon;
+  final Color? color;
+
+  CategoryModel({required this.category, required this.icon, this.color});
+}
+
+ List<CategoryModel> categoriesList = [
+  CategoryModel(category: "Sports", icon: AppAssets.sportsSvg,color: Colors.transparent),
+  CategoryModel(category: "Music", icon: AppAssets.musicSvg,color: Colors.transparent),
+  CategoryModel(category: "Food", icon: AppAssets.foodSvg,color: Colors.transparent),
+  CategoryModel(category: "Art", icon: AppAssets.artSvg,color: Colors.transparent),
+];
