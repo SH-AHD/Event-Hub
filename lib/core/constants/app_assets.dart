742777ef866b4da1358656f3c724fd31b268a4f7
Moamen Ragab <moamenragab66@gmail.com>
2026-02-22 02:00:42 +0200

    add an asset

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,34 +1,32 @@
 class AppAssets {
+  //icons
+  static const String logoSvg = 'assets/icons/logo.svg';
+  static const String eventSvg = 'assets/icons/Calendar.svg';
+  static const String exploreSvg = 'assets/icons/explore.svg';
+  static const String profileSvg = 'assets/icons/Profile.svg';
+  static const String mapSvg = 'assets/icons/Location.svg';
+  static const String addSvg = 'assets/icons/add.svg';
+  static const String arrowSvg = 'assets/icons/arrow.svg';
+  static const String saveSvg = 'assets/icons/save.svg';
+  static const String proSvg = 'assets/icons/pro.svg';
+  static const String musicSvg = 'assets/icons/music.svg';
+  static const String foodSvg = 'assets/icons/food.svg';
+  static const String filterSvg = 'assets/icons/filter.svg';
+  static const String downArrowSvg = 'assets/icons/down_arrow.svg';
+  static const String searchSvg = 'assets/icons/search.svg';
+  static const String drawerMenuSvg = 'assets/icons/drawer_menu.svg';
+  static const String sportsSvg = 'assets/icons/sports.svg';
 
-//icons
-static const String logoSvg='assets/icons/logo.svg';
-static const String eventSvg='assets/icons/Calendar.svg';
-static const String exploreSvg='assets/icons/explore.svg';
-static const String profileSvg='assets/icons/Profile.svg';
-static const String mapSvg='assets/icons/Location.svg';
-static const String addSvg='assets/icons/add.svg';
-static const String arrowSvg='assets/icons/arrow.svg';
-static const String saveSvg='assets/icons/save.svg';
-static const String proSvg='assets/icons/pro.svg';
-static const String musicSvg='assets/icons/music.svg';
-static const String foodSvg='assets/icons/food.svg';
-static const String filterSvg='assets/icons/filter.svg';
-static const String downArrowSvg='assets/icons/down_arrow.svg';
-static const String searchSvg='assets/icons/search.svg';
-static const String drawerMenuSvg='assets/icons/drawer_menu.svg';
-static const String sportsSvg='assets/icons/sports.svg';
-
-
-//imgs
-static const String inviteImg='assets/imgs/inviteImg.png';
-static const String joMelanoImg='assets/imgs/jo_melano.png';
-static const String virtualEveningImg='assets/imgs/virtual_evening.png'; 
-static const String womensImg='assets/imgs/womens.png';
-static const String internationalKidsImg='assets/imgs/international_kids.png';
-static const String collectivityImg='assets/imgs/collectivity.png';
-static const String internationalGalaImg='assets/imgs/international_gala.png';
-
-
+  //imgs
+  static const String inviteImg = 'assets/imgs/inviteImg.png';
+  static const String joMelanoImg = 'assets/imgs/jo_melano.png';
+  static const String virtualEveningImg = 'assets/imgs/virtual_evening.png';
+  static const String womensImg = 'assets/imgs/womens.png';
+  static const String internationalKidsImg =
+      'assets/imgs/international_kids.png';
+  static const String collectivityImg = 'assets/imgs/collectivity.png';
+  static const String internationalGalaImg =
+      'assets/imgs/international_gala.png';
 
   //icons
   static const String bellSvg = 'assets/icons/bell.svg';
@@ -50,7 +48,7 @@ static const String internationalGalaImg='assets/imgs/international_gala.png';
   static const String bookmark1Svg = 'assets/icons/bookmark1.svg';
   static const String calendarevSvg = 'assets/icons/CalendarEv.svg';
   static const String locationevSvg = 'assets/icons/LocationEv.svg';
-  static const String pinPutSvg = 'assets/icons/Profile.png';
+  static const String pinPutSvg = 'assets/icons/pinputSvg.svg';
   //imgs
   static const String mapViewImg = 'assets/imgs/MapView.png';
   static const String eventHubImg = 'assets/imgs/EventHubImg.png';
